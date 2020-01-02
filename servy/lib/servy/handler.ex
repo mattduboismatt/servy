f13aeca320e7e80940f71f1e9ae47c6669194cd2
Matt DuBois <mattduboismatt@gmail.com>
2020-01-02 15:48:46 -0800

    first request done

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -14,23 +14,27 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    # TODO: Parse the request string into a map:
-    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
-    # TODO: Create a new map that also has the response body:
-    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+    # conv = Map.put(conv, :resp_body, "Bears, Lions, Tigers")
+    %{conv | resp_body: "Bears, Lions, Tigerzzz"}
   end
 
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
@@ -42,14 +46,6 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
-
-# expected_response = """
-# HTTP/1.1 200 OK
-# Content-Type: text/html
-# Content-Length: 20
-
-# Bears, Lions, Tigers
-# """
 
 response = Servy.Handler.handle(request)
 IO.puts(response)
